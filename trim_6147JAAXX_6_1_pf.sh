c4adf6887f69c8a8f102929a3b52ce838af89dfa
Likit Preeyanon <preeyano@msu.edu>
2013-10-01 23:58:57 -0400

    add trimming job scripts

---
+++ trim_6147JAAXX_6_1_pf.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=16gb,walltime=12:00:00
+#PBS -m abe
+#PBS -N condetri_line7u
+#PBS -A ged-intel11
+
+cd /mnt/ls12/preeyanon/gimme/
+condetri_v2.1.pl -fastq1=6147JAAXX_6_1_pf.fastq -sc=33 -cutfirst 10
