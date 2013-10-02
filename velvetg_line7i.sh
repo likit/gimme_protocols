97ae821f2b4906a519e78cad4fe06d910458aa37
Likit Preeyanon <preeyano@msu.edu>
2013-10-02 16:52:26 -0400

    add velvetg job scripts

---
+++ velvetg_line7i.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=64gb,walltime=48:00:00
+#PBS -m abe
+#PBS -v DATAPATH
+#PBS -N Velvetg_line7i_global
+#PBS -t 21,23,25,27,29,31
+
+cd $DATAPATH
+velvetg line7i_global_${PBS_ARRAYID} -read_trkg yes -unused_reads yes
