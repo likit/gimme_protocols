054fe666dd003584319bd996cbda4d9a775635e8
Likit Preeyanon <preeyano@msu.edu>
2014-07-19 18:13:27 -0400

    removed deprecated oases job scripts

--- oases_line7i.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -login
-#PBS -l nodes=1:ppn=1,mem=64gb,walltime=48:00:00
-#PBS -m abe
-#PBS -v DATAPATH
-#PBS -N Oases_line7i_global
-#PBS -t 21,23,25,27,29,31
-
-cd $DATAPATH
-oases line7i_global_${PBS_ARRAYID} -unused_reads yes
