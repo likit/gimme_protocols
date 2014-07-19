925f09c3ca0d12ac965b9e4271a78cedb25d3c8f
Likit Preeyanon <preeyano@msu.edu>
2014-07-19 11:44:26 -0400

    added some scripts

---
+++ blat_job.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=24gb,walltime=24:00:00
+#PBS -M preeyano@msu.edu
+#PBS -m abe
+#PBS -N Blat_${PBS_JOBID}
+
+cd ${PBS_O_WORKDIR}
+blat -noHead -extendThroughN -mask=lower -out=psl chick_3.2bit ${input} ${input}.psl
