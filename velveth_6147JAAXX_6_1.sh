458ef509e349e4b419342a29fe2270c2cd528a1d
Likit Preeyanon <preeyano@msu.edu>
2014-07-19 00:50:35 -0400

    deleted old job files
    added quality_trim job file

--- velveth_6147JAAXX_6_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -login
-#PBS -l nodes=1:ppn=1,mem=48gb,walltime=24:00:00
-#PBS -m abe
-#PBS -N Velveth_line7u_global
-#PBS -v DATAPATH
-
-cd $DATAPATH
-velveth line7u_global 21,33,2 -fastq -short 6147JAAXX_6_1_pf_trim.fastq
