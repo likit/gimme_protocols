98352958960bb5fd08d371864bbb9e2afb42b7fb
Likit Preeyanon <preeyano@msu.edu>
2014-07-21 11:47:10 -0400

    added new scripts

---
+++ cdhit_job.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=4,mem=24gb,walltime=24:00:00
+#PBS -M preeyano@msu.edu
+#PBS -m abe
+#PBS -N CDHIT_gimme_${PBS_JOBID}
+
+module load CDHIT/4.5.6
+
+cd ${PBS_O_WORKDIR}
+cd-hit-est -T 4 -d 0 -c ${c} -M 24000 -i ${input} -o ${output}
---
+++ oases_mouse.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -login
+#PBS -l nodes=1:ppn=1,mem=256gb,walltime=24:00:00
+#PBS -M preeyano@msu.edu
+#PBS -m abe
+#PBS -N Oases_gimme_${PBS_JOBID}
+#PBS -t 21,23,25,27,29,31
+
+cd ${PBS_O_WORKDIR}
+oases mouse_global_${PBS_ARRAYID} -ins_length 150
